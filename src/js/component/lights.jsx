cb5159691d2419cc6fced649a4d16c8855bdb479
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-11-07 13:15:51 +0000

    random button

--- src/js/component/lights.jsx
+++ src/js/component/lights.jsx
@@ -5,9 +5,13 @@ const Lights =() =>{
     const [red, setRed] = useState('red')
     const [ambar, setAmbar] = useState('ambar')
     const [verde, setVerde] = useState('verde')
-    
 
-    function changeColourRed (){
+    
+    const [displayPurple, setDisplayPurple] = useState("none")
+
+    
+//funtion to get the lights when clicks
+    function changeColourRed(){
         if(red === "red"){
             setRed('glowRed')
             setAmbar('ambar')
@@ -18,7 +22,7 @@ const Lights =() =>{
             setVerde('verde')
         }
     }
-    function changeColourAmbar (){
+    function changeColourAmbar(){
         if(ambar === "ambar"){
             setAmbar('glowAmbar')
             setRed('red')
@@ -29,7 +33,7 @@ const Lights =() =>{
             setVerde('verde')
         }     
     }
-    function changeColourVerde (){
+    function changeColourVerde(){
         if(verde === "verde"){
             setVerde('glowGreen')
             setRed('red')
@@ -39,15 +43,59 @@ const Lights =() =>{
             setAmbar('ambar')
             setVerde('verde')             
         }
-        
     }
 
+    const changeColorRed = () => {
+		if (red === "red") {
+			setRed("ambar");
+		} else if (red === "ambar") {
+			setRed("verde");
+
+		} else if (ambar === "ambar") {
+			setAmbar("verde");
+        } else if (ambar === "verde") {
+			setAmbar("red");
+
+        } else if (verde === "verde") {
+			setVerde("red");
+        } else if (verde === "red") {
+			setVerde("ambar");
+
+	    } else {
+            setRed("red");
+            setAmbar("ambar");
+            setVerde("verde");
+        }
+    }
+//function to get a random light traffic
+const addPurple = () => {
+    setDisplayPurple("block");
+    setRed("purple");
+}
+    
     return (
+        <>
         <div className="traffic_background">
-            <button className={red} onClick={()=>changeColourRed()}></button>
-			<button className={ambar} onClick={()=>changeColourAmbar()}></button>
-			<button className={verde} onClick={()=>changeColourVerde()}></button>
+            <button 
+                className={red} 
+                onClick={()=>changeColourRed()}>
+            </button>
+			<button 
+                className={ambar} 
+                onClick={()=>changeColourAmbar()}>
+            </button>
+			<button 
+                className={verde} 
+                onClick={()=>changeColourVerde()}>
+            </button>
         </div>
+        <div className={"purple " + (red === "purple" ? "glow" : "")} style={{ display: displayPurple }} onClick={addPurple}></div>
+				
+        <div>
+            <button className="btn btn-primary m-4" onClick={changeColorRed}>Change</button>
+			<button className="btn btn-success m-4" onClick={addPurple}>Añadir Púrpura</button>
+        </div>
+        </>
     )
 }
 
